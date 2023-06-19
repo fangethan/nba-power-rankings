90f5d2b4e26c4cf15d7d25888d69c7e4e80faab1
Ethan Fang <ethan.fang@gmail.com>
2023-06-19 18:44:23 +1000

    Created handleDeletePlayers to call id of players

--- src/components/SinglePlayer.tsx
+++ src/components/SinglePlayer.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from "react-router-dom"
+import {AiFillDelete} from "react-icons/ai"
 
 interface SinglePlayerProps {
     name: string;
@@ -7,12 +8,20 @@ interface SinglePlayerProps {
 }
 
 export const SinglePlayer: React.FC<SinglePlayerProps> = ({name, id}) => {
+    
+    const handleDeletePlayer = (id: string) => {
+        console.log(id)
+    }
+
     return (
         <div className="single_player">
             <Link to={`/player/${id}`}>
                 <h2>{name}</h2>
                 <p>New York Knicks</p>
             </Link>
+            <span className="icon" onClick={() => handleDeletePlayer(id)}>
+                <AiFillDelete/>
+            </span>
         </div>
     )
 }
