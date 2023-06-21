0e13f05f563a07e47a48a28e98bb80b2d386ed52
Ethan Fang <ethan.fang@gmail.com>
2023-06-21 21:22:58 +1000

    Rename SinglePlayer to PlayerDetails

--- src/components/PlayerList.tsx
+++ src/components/PlayerList.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react' 
 import { PlayerModel } from '../PlayerModel'
-import { SinglePlayer } from './SinglePlayer'
+import { SinglePlayer } from './PlayerDetails'
 import {Link} from "react-router-dom"
 import {AiFillDelete} from "react-icons/ai"
 
