3909de479be3125ba87637c13145a8eb09dac1e9
Ethan Fang <ethan.fang@gmail.com>
2023-07-08 01:08:37 +1000

    Fix up search bar styling

--- src/components/InputPlayerField.tsx
+++ src/components/InputPlayerField.tsx
@@ -11,18 +11,20 @@ export const InputPlayerField: React.FC<InputPlayerFieldProps> = ({playerName, s
     const playerInputRef = useRef<HTMLInputElement>(null)
 
     return (
-        <form className="player_input" onSubmit={(e) => {
-            handleAddPlayer(e)
-            playerInputRef.current?.blur()
-        }}> 
-            <input 
-                ref={playerInputRef}
-                type="input" 
-                value={playerName} 
-                onChange={(e)=>setplayerName(e.target.value)} 
-                placeholder="Enter a player's name" 
-                className="player_input_box" />
-            <button className="player_input_submit" type="submit">Go</button>
-        </form> 
+        <div className='player_input_container'>
+            <form className="player_input" onSubmit={(e) => {
+                handleAddPlayer(e)
+                playerInputRef.current?.blur()
+            }}> 
+                <input 
+                    ref={playerInputRef}
+                    type="input" 
+                    value={playerName} 
+                    onChange={(e)=>setplayerName(e.target.value)} 
+                    placeholder="Enter a player's name" 
+                    className="player_input_box" />
+                <button className="player_input_submit" type="submit">Go</button>
+            </form> 
+        </div>
     )
 }
