8fd2126a616b47d3ba8bf9ba7ba6f06875342e14
Ethan Fang <ethan.fang@gmail.com>
2023-07-09 12:22:54 +1000

    Move teamLogos into separate ts file

--- src/components/PlayerList.tsx
+++ src/components/PlayerList.tsx
@@ -4,73 +4,7 @@ import { SinglePlayer } from './PlayerDetails'
 import {Link} from "react-router-dom"
 import {AiFillDelete} from "react-icons/ai"
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-
-
-type TeamLogoKey =
-  | "atlantahawks"
-  | "bostonceltics"
-  | "brooklynnets"
-  | "charlottehornets"
-  | "chicagobulls"
-  | "clevelandcavaliers"
-  | "dallasmavericks"
-  | "denvernuggets"
-  | "detroitpistons"
-  | "goldenstatewarriors"
-  | "houstonrockets"
-  | "indianapacers"
-  | "laclippers"
-  | "losangeleslakers"
-  | "memphisgrizzlies"
-  | "miamiheat"
-  | "milwaukeebucks"
-  | "minnesotatimberwolves"
-  | "neworleanspelicans"
-  | "newyorkknicks"
-  | "oklahomacitythunder"
-  | "orlandomagic"
-  | "philadelphia76ers"
-  | "phoenixsuns"
-  | "portlandtrailblazers"
-  | "sacramentokings"
-  | "sanantoniospurs"
-  | "torontoraptors"
-  | "utahjazz"
-  | "washingtonwizards";
-
-const teamLogos: Record<TeamLogoKey, string> = {
-  atlantahawks: require("../images/team_logos/atl_hawks_logo.png"),
-  bostonceltics: require("../images/team_logos/boston_logo.png"),
-  brooklynnets: require("../images/team_logos/brooklyn_logo.png"),
-  charlottehornets: require("../images/team_logos/charlotte_logo.png"),
-  chicagobulls: require("../images/team_logos/chicago_logo.png"),
-  clevelandcavaliers: require("../images/team_logos/cavs_logo.png"),
-  dallasmavericks: require("../images/team_logos/dallas_logo.png"),
-  denvernuggets: require("../images/team_logos/denver_logo.png"),
-  detroitpistons: require("../images/team_logos/detroit_logo.png"),
-  goldenstatewarriors: require("../images/team_logos/warriors_logo.png"),
-  houstonrockets: require("../images/team_logos/rockets_logo.png"),
-  indianapacers: require("../images/team_logos/pacers_logo.png"),
-  laclippers: require("../images/team_logos/clippers_logo.png"),
-  losangeleslakers: require("../images/team_logos/lakers_logo.png"),
-  memphisgrizzlies: require("../images/team_logos/memphis_logo.png"),
-  miamiheat: require("../images/team_logos/miami_logo.png"),
-  milwaukeebucks: require("../images/team_logos/bucks_logo.png"),
-  minnesotatimberwolves: require("../images/team_logos/timberwolves_logo.png"),
-  neworleanspelicans: require("../images/team_logos/pelicans_logo.png"),
-  newyorkknicks: require("../images/team_logos/knicks_logo.png"),
-  oklahomacitythunder: require("../images/team_logos/okc_logo.png"),
-  orlandomagic: require("../images/team_logos/magic_logo.png"),
-  philadelphia76ers: require("../images/team_logos/76ers_logo.png"),
-  phoenixsuns: require("../images/team_logos/suns_logo.png"),
-  portlandtrailblazers: require("../images/team_logos/blazers_logo.png"),
-  sacramentokings: require("../images/team_logos/kings_logo.png"),
-  sanantoniospurs: require("../images/team_logos/spurs_logo.png"),
-  torontoraptors: require("../images/team_logos/raptors_logo.png"),
-  utahjazz: require("../images/team_logos/jazz_logo.png"),
-  washingtonwizards: require("../images/team_logos/wizards_logo.png"),
-};
-
+import { teamLogos } from './TeamLogos'
 
 interface PlayerListProps {
     players: PlayerModel[]
